ab96a7b8a7e044ddb71cb444cd0cbfbf3db9eba9
Binh Diep <haibinh232@gmail.com>
2023-10-04 23:53:18 +0700

    user info api

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from apps.users.api import views
 
 urlpatterns = [
+    path("user-info/", views.UserInfoView.as_view()),
     path("me/", views.UsersProfileView.as_view()),
     path("exists/", views.UserExistView.as_view(), name="user-exists"),
     path("password-reset/", views.PasswordResetView.as_view()),
