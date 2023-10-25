17778ae26593bf8c8763025ca68956ff0d040b14
Binh Diep <haibinh232@gmail.com>
2023-10-25 19:33:15 +0700

    import db

--- apps/system/management/commands/import_db.py
+++ apps/system/management/commands/import_db.py
@@ -4,5 +4,10 @@ from apps.system.services.database_services import import_database
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('--path', type=str, default="data_server.json", help='Specify a custom argument')
+
     def handle(self, *args, **options):
-        import_database("data_server.json")
+        path = options['path']
+        import_database(path)
+
--- apps/system/services/database_services.py
+++ apps/system/services/database_services.py
@@ -1,5 +1,10 @@
-from apps.system.model_choices import import_db_model
+from typing import Dict
+import subprocess
 import json
+
+from django.contrib.auth.models import Permission, ContentType
+
+from apps.system.model_choices import import_db_model
 
 
 def apply_action(model, data: dict, action: str, extra_data, extra_action: str = "", ):
@@ -30,55 +35,76 @@ def apply_action(model, data: dict, action: str, extra_data, extra_action: str =
     return data
 
 
+def get_field_type(fields, model_class):
+    m2m_field = []
+    json_field = []
+    array_field = []
+    fk_key_field = []
+
+    for field in fields:
+        field_type = model_class._meta.get_field(field).get_internal_type()
+        if field_type == "ManyToManyField":
+            m2m_field.append(field)
+        elif field_type == "JSONField":
+            json_field.append(field)
+        elif field_type == "ArrayField":
+            array_field.append(field)
+        elif field_type == "ForeignKey" or field_type == "OneToOneField":
+            fk_key_field.append(field)
+
+    return fk_key_field, m2m_field, array_field, json_field
+
+
+def handle_data(data, model_name, model_class):
+    res = {}
+
+    items = [item for item in data if item["model"] == model_name]
+    if not items:
+        return
+
+    all_fields_name = [field.name for field in model_class._meta.get_fields()]
+    model_field = set(items[0]["fields"]).intersection(set(all_fields_name))
+    fk_key_field, m2m_field, array_field, json_field = get_field_type(model_field, model_class)
+
+    for item in items:
+        pk = item["pk"]
+        attr = item["fields"]
+        m2m = {}
+        for field in fk_key_field:
+            attr[field + "_id"] = attr.pop(field)
+        for field in m2m_field:
+            m2m[field] = attr.pop(field)
+        for field in array_field:
+            attr[field] = json.loads(attr[field]) if isinstance(attr[field], str) else attr[field]
+        for field in json_field:
+            attr[field] = json.loads(attr[field]) if isinstance(attr[field], str) else attr[field]
+
+        res[pk] = {
+            "instance": model_class(pk=pk, **attr),
+            "m2m": m2m
+        }
+
+    return res
+
+
+def store_model_data(data: Dict, model_class):
+    if not data:
+        return
+
+    model_class.objects.bulk_create([info["instance"] for info in data.values()])
+    for info in data.values():
+        instance = info["instance"]
+        for m2m_field, val in info["m2m"].items():
+            exec(f"instance.{m2m_field}.set({val})")
+
+
 def import_database(json_file):
     data = json.load(open(json_file, "r"))
-
     choices = import_db_model
 
+    Permission.objects.all().delete()
+    ContentType.objects.all().delete()
+
     for key, model in choices.items():
-        items = [item for item in data if item["model"] == key]
-        field_names = [field.name for field in model._meta.get_fields()]
-        model_field = set(items[0]["fields"]).intersection(set(field_names))
-
-        m2m_field = []
-        json_field = []
-        array_field = []
-        fk_key_field = []
-        for field in model_field:
-            field_type = model._meta.get_field(field).get_internal_type()
-            if field_type == "ManyToManyField":
-                m2m_field.append(field)
-            elif field_type == "JSONField":
-                json_field.append(field)
-            elif field_type == "ArrayField":
-                array_field.append(field)
-            elif field_type == "ForeignKey" or field_type == "OneToOneField":
-                fk_key_field.append(field)
-
-        for item in items:
-            for field in json_field:
-                value = item["fields"][field]
-                if isinstance(value, str):
-                    item["fields"][field] = json.loads(value) if value is not None else value
-            for field in array_field:
-                value = item["fields"][field]
-                item["fields"][field] = json.loads(value) if value is not None else value
-            for field in fk_key_field:
-                item["fields"][field + "_id"] = item["fields"].pop(field)
-
-        if m2m_field:
-            for item in items:
-                m2m_value = {}
-                fields = item["fields"]
-                for field in m2m_field:
-                    m2m_value[field] = fields.pop(field)
-                record = model.objects.create(pk=item["pk"], **fields)
-                for key, val in m2m_value.items():
-                    exec(f"record.{key}.set({val})")
-
-        else:
-            for dt in items:
-                print(dt["fields"]["action_time"])
-                model.objects.create(pk=dt["pk"], **dt["fields"])
-            obj_create = [model(pk=dt["pk"], **dt["fields"]) for dt in items]
-            model.objects.bulk_create(obj_create)
+        dict_data = handle_data(data, key, model)
+        store_model_data(dict_data, model)
